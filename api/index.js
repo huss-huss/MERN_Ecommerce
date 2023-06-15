7a9ce43700b15268a019439955453aa63bb7f1cb
Mintesnote Bankisra <lordminty2000@gmail.com>
2023-06-15 15:23:21 +0300

    changing route

--- api/index.js
+++ api/index.js
@@ -1,56 +1,78 @@
-import express from 'express'
-import authRoutes from './routes/auth.js'
-// import userRoutes from "./routes/users.js";
-import userRoutes from './routes/user.js'
-import productRoutes from './routes/product.js'
-import productCategoryRoutes from './routes/product_category.js'
-import orderRoutes from './routes/order.js'
-import cartRoutes from './routes/cart.js'
-import reviewRoutes from './routes/review.js'
-import couponRoutes from './routes/coupon.js'
-import wishlistRoutes from './routes/wishlist.js'
-import { db } from './db.js'
+import express from 'express';
+import addressRoutes from './address-routes'; 
+import auth from './routes/auth.js';
+import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import productCategoryRoutes from './routes/productCategoryRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import orderStatusRoutes from './routes/orderStatusRoutes.js';
+import addressRoutes from './routes/addressRoutes.js';
+import orderItemRoutes from './routes/orderItemRoutes.js';
+import couponRoutes from './routes/couponRoutes.js';
+import customProductRoutes from './routes/customProductRoutes.js';
+import paymentRoutes from './routes/paymentRoutes.js';
+import productImageRoutes from './routes/productImageRoutes.js';
+import reviewRoutes from './routes/reviewRoutes.js';
+import roleRoutes from './routes/roleRoutes.js';
+import shoppingCartItemRoutes from './routes/shoppingCartItemRoutes.js';
+import shoppingCartRoutes from './routes/shoppingCartRoutes.js';
+import userUploadRoutes from './routes/userUploadRoutes.js';
+import wishlistItemRoutes from './routes/wishlistItemRoutes.js';
+import wishlistRoutes from './routes/wishlistRoutes.js';
 
-import cookieParser from 'cookie-parser'
-import multer from 'multer'
+import { db } from './db.js';
+import cookieParser from 'cookie-parser';
+import multer from 'multer';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../client/public/upload')
+    cb(null, '../client/public/upload');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname)
+    cb(null, Date.now() + file.originalname);
   },
-})
+});
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
 app.post('/api/upload', upload.single('file'), function (req, res) {
-  const file = req.file
-  res.status(200).json(file.filename)
-})
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
-app.use('/api', authRoutes)
-app.use('/api', userRoutes)
-app.use('/api', productRoutes)
-app.use('/api', productCategoryRoutes)
-app.use('/api', orderRoutes)
-app.use('/api', cartRoutes)
-app.use('/api', reviewRoutes)
-app.use('/api', couponRoutes)
-app.use('/api', wishlistRoutes)
+app.use('/api',addressRoutes)
+app.use('/api', auth);
+app.use('/api', userRoutes);
+app.use('/api', productRoutes);
+app.use('/api', productCategoryRoutes);
+app.use('/api', orderRoutes);
+app.use('/api', orderStatusRoutes);
+app.use('/api', addressRoutes);
+app.use('/api', orderItemRoutes);
+app.use('/api', couponRoutes);
+app.use('/api', customProductRoutes);
+app.use('/api', paymentRoutes);
+app.use('/api', productImageRoutes);
+app.use('/api', reviewRoutes);
+app.use('/api', roleRoutes);
+app.use('/api', shoppingCartItemRoutes);
+app.use('/api', shoppingCartRoutes);
+app.use('/api', userUploadRoutes);
+app.use('/api', wishlistItemRoutes);
+app.use('/api', wishlistRoutes);
 
 db.connect((error) => {
   if (error) {
-    console.log('Error connecting to MySQL database: ' + error)
+    console.log('Error connecting to MySQL database: ' + error);
   } else {
-    console.log('Connected to MySQL database')
+    console.log('Connected to MySQL database');
   }
-})
+});
+
 app.listen(8800, () => {
-  console.log('Server is Running on port 8800!')
-})
+  console.log('Server is running on port 8800!');
+});
